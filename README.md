ef786f046f717c9d78924dfe5ccd52809ac40ca0
Akito Mizuno <mznakt@gmail.com>
2018-06-25 02:15:00 +0900

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## iOSTest
+iOSの練習
+
+by [10分で試せる！ Swiftを使った初めてのiOSアプリ開発入門 - ICS MEDIA](https://ics.media/entry/6439)
---
+++ iOSTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOSTest
+//
+//  Created by 水野陽斗 on 2018/06/25.
+//  Copyright © 2018 水野陽斗. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
